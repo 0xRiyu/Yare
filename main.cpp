8dc5c4be762724c07821081bf682dd9ad53dd54e
phaserush <leozhang888@yahoo.ca>
2019-08-13 18:43:48 -0700

    Added Jetbrains/Clion files to .gitignore

--- main.cpp
+++ main.cpp
@@ -3,7 +3,13 @@
 //
 #include <iostream>
 
+struct T{
+    int_fast32_t f = 1;
+};
+
 int main() {
     std::cout << "Hello World!" << std::endl;
+    T t = T();
+    std::cout << t.f << std::endl;
     return 0;
 }
