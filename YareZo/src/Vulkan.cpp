d2190d0e84ab0ddb9906d36f42415b4122ffde49
Resoona <drewcornfield@gmail.com>
2019-11-10 20:15:09 -0500

    Validation Layers I guess

--- YareZo/src/Vulkan.cpp
+++ YareZo/src/Vulkan.cpp
@@ -9,6 +9,43 @@
 namespace Yarezo {
 
     std::unique_ptr<VkInstance> Vulkan::InitVulkan() {
+
+        auto instance = createInstance();
+
+        return std::make_unique<VkInstance>(instance);
+    }
+
+    bool Vulkan::checkValidationLayerSupport() {
+        uint32_t layerCount;
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+        std::vector<VkLayerProperties> availableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+        for (const char* layerName : validationLayers) {
+            bool layerFound = false;
+
+            for (const auto& layerProperties : availableLayers) {
+                if (strcmp(layerName, layerProperties.layerName) == 0) {
+                    layerFound = true;
+                    break;
+                }
+            }
+
+            if (!layerFound) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    VkInstance Vulkan::createInstance() {
+        if (enableValidationLayers && !checkValidationLayerSupport()) {
+            YZ_ERROR("VK Instance was unable to be created, no validation layers available");
+            throw std::runtime_error("validation layers requested, but not available!");
+        }
+
         VkInstance instance;
 
         VkApplicationInfo appInfo = {};
@@ -23,12 +60,23 @@ namespace Yarezo {
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
 
-        uint32_t glfwExtensionCount = 0;
-        const char** glfwExtensions;
-        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        auto extensions = getRequiredExtensions();
+        createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+        createInfo.ppEnabledExtensionNames = extensions.data();
 
-        createInfo.enabledExtensionCount = glfwExtensionCount;
-        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo;
+        if (enableValidationLayers) {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+
+            populateDebugMessengerCreateInfo(debugCreateInfo);
+            createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debugCreateInfo;
+        } else {
+            createInfo.enabledLayerCount = 0;
+
+            createInfo.pNext = nullptr;
+        }
 
         createInfo.enabledLayerCount = 0;
 
@@ -38,8 +86,35 @@ namespace Yarezo {
         }
         YZ_INFO("VK Instance WAS able to be created");
 
-        return std::make_unique<VkInstance>(instance);
+        return instance;
     }
 
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
+        YZ_INFO("validation layer: " + std::string(pCallbackData->pMessage));
+        return VK_FALSE;
+    }
+
+
+    void Vulkan::populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
+        createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        createInfo.pfnUserCallback = debugCallback;
+    }
+
+    std::vector<const char*> Vulkan::getRequiredExtensions() {
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+        if (enableValidationLayers) {
+            extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        }
+
+        return extensions;
+    }
 
 }
--- YareZo/src/Vulkan.h
+++ YareZo/src/Vulkan.h
@@ -7,14 +7,33 @@
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <vector>
 #include <memory>
+#include <iostream>
 
 namespace Yarezo {
 
     class Vulkan {
     public:
         static std::unique_ptr<VkInstance> InitVulkan();
+        static bool checkValidationLayerSupport();
+
+    private:
+        static VkInstance createInstance();
+        static void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+        static std::vector<const char*> getRequiredExtensions();
     };
+
+        const std::vector<const char*> validationLayers = {
+                "VK_LAYER_KHRONOS_validation"
+        };
+
+        #ifdef NDEBUG
+            const bool enableValidationLayers = false;
+        #else
+            const bool enableValidationLayers = true;
+        #endif
+
 }
 
 
