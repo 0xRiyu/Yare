a89c368c32c0363a837ab3910396c6370ef9c58e
Resoona <cornfield_drew@yahoo.com>
2020-03-16 00:09:24 -0400

    Actually add the model this time

--- YareZo/Platform/Vulkan/Vk_Renderpass.cpp
+++ YareZo/Platform/Vulkan/Vk_Renderpass.cpp
@@ -87,7 +87,7 @@ namespace Yarezo {
             renderPassInfo.renderArea.offset = { 0, 0 };
             renderPassInfo.renderArea.extent = swapchain->getExtent();
             std::array<VkClearValue, 2> clearValues = {};
-            clearValues[0].color = { 0.0f, 0.0f, 0.0f, 1.0f };
+            clearValues[0].color = { 0.7f, 0.8f, 0.9f, 1.0f };
             clearValues[1].depthStencil = { 1.0f, 0 };
             renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
             renderPassInfo.pClearValues = clearValues.data();
